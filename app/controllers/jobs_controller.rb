ca85df3e5ec84a69e5878258bd52c8964550299a
onjoroge1 <kim.njo@gmail.com>
2015-09-05 00:10:59 +0000

    added categories

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,12 @@ class JobsController < ApplicationController
 
   before_action :find_job, only: [:show, :edit, :destroy, :update]
   def index
-    @jobs = Job.all.order("created_at DESC")
+    if params[:category].blank?
+			@jobs = Job.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@jobs = Job.where(category_id: @category_id).order("created_at DESC")
+		end
   end
 
   def new
@@ -44,7 +49,7 @@ class JobsController < ApplicationController
   end
 
   def job_params
-    params.require(:job).permit(:title, :description, :url, :company)
+    params.require(:job).permit(:title, :description, :url, :company, :category_id)
   end
 
 end
